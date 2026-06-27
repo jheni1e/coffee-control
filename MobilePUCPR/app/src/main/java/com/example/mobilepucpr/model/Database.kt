ab5a2c38365d22627627af5f0d7ad4350cc85303
Jhenie <highhalma@gmail.com>
2026-06-27 09:55:17 -0300

    FEAT: database connected and CRUD implemented

---
+++ MobilePUCPR/app/src/main/java/com/example/mobilepucpr/model/Database.kt
@@ -0,0 +1,150 @@
+package com.example.mobilepucpr.model
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class Database(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        const val DATABASE_NAME = "coffees.db"
+        const val DATABASE_VERSION = 1
+        const val DB_TABLE = "coffeeItem"
+        const val DB_FIELD_ID = "id"
+        const val DB_FIELD_NAME = "name"
+        const val DB_FIELD_DESCRIPTION = "description"
+        const val DB_FIELD_CATEGORY = "category"
+        const val DB_FIELD_PRICE = "price"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val sql = "CREATE TABLE IF NOT EXISTS $DB_TABLE (" +
+                "$DB_FIELD_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$DB_FIELD_NAME TEXT, " +
+                "$DB_FIELD_DESCRIPTION TEXT, " +
+                "$DB_FIELD_CATEGORY TEXT, " +
+                "$DB_FIELD_PRICE FLOAT);"
+
+        val db = db ?: return
+
+        db.beginTransaction()
+        try {
+            db.execSQL(sql)
+            db.setTransactionSuccessful()
+        }
+        catch (e: Exception) {
+            Log.d("CoffeesApp", e.localizedMessage)
+        }
+        finally {
+            db.endTransaction()
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun getAllCoffees(): MutableList<CoffeeItem> {
+        var coffees = mutableListOf<CoffeeItem>()
+        val db = readableDatabase
+        val cursor = db.query(
+            DB_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            DB_FIELD_NAME
+        )
+        with(cursor) {
+            while(moveToNext()) {
+                val id = getLong(getColumnIndexOrThrow(DB_FIELD_ID))
+                val name = getString(getColumnIndexOrThrow(DB_FIELD_NAME))
+                val description = getString(getColumnIndexOrThrow(DB_FIELD_DESCRIPTION))
+                val category = getString(getColumnIndexOrThrow(DB_FIELD_CATEGORY))
+                val price = getFloat(getColumnIndexOrThrow(DB_FIELD_PRICE))
+                val coffeeItem = CoffeeItem(name, description, category, price)
+                coffeeItem.id = id
+                coffees.add(coffeeItem)
+            }
+        }
+
+        return coffees
+    }
+
+    fun addCoffeeItem(item: CoffeeItem): Long {
+        var id: Long = 0
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(DB_FIELD_NAME, item.name)
+            put(DB_FIELD_DESCRIPTION, item.description)
+            put(DB_FIELD_CATEGORY, item.category)
+            put(DB_FIELD_PRICE, item.price)
+        }
+
+        db.beginTransaction()
+        try {
+            id = db.insert(DB_TABLE, null, values)
+            db.setTransactionSuccessful()
+        }
+        catch (e: Exception) {
+            Log.d("CoffeesApp", e.localizedMessage)
+        }
+        finally {
+            db.endTransaction()
+        }
+
+        return id
+    }
+
+    fun editCoffeeItem(item: CoffeeItem): Int {
+        var count = 0
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(DB_FIELD_NAME, item.name)
+            put(DB_FIELD_DESCRIPTION, item.description)
+            put(DB_FIELD_CATEGORY, item.category)
+            put(DB_FIELD_PRICE, item.price)
+        }
+
+        val selection = "$DB_FIELD_ID = ?"
+        val arg = arrayOf(item.id.toString())
+
+        db.beginTransaction()
+        try {
+            count = db.update(DB_TABLE, values, selection, arg)
+            db.setTransactionSuccessful()
+        }
+        catch (e: Exception) {
+            Log.d("CoffeesApp", e.localizedMessage)
+        }
+        finally {
+            db.endTransaction()
+        }
+
+        return count
+    }
+
+    fun removeCoffeeItem(item: CoffeeItem): Int {
+        var count = 0
+        val db = writableDatabase
+
+        val selection = "$DB_FIELD_ID = ?"
+        val arg = arrayOf(item.id.toString())
+
+        db.beginTransaction()
+        try {
+            count = db.delete(DB_TABLE, selection, arg)
+            db.setTransactionSuccessful()
+        }
+        catch (e: Exception) {
+            Log.d("CoffeesApp", e.localizedMessage)
+        }
+        finally {
+            db.endTransaction()
+        }
+
+        return count
+    }
+}
